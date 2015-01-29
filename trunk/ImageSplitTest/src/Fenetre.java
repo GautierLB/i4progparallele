304715760dec9f4e8ca68b13f21a0e326192b73a
gautier.lb@gmail.com <gautier.lb@gmail.com@89debde2-f5b9-c917-bf0e-35cadc44cf30>
2015-01-29 10:16:52 +0000

    ** LE BISSONNAIS Gautier **
    - Start & pause propres des threads

--- trunk/ImageSplitTest/src/Fenetre.java
+++ trunk/ImageSplitTest/src/Fenetre.java
@@ -16,51 +16,51 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 
+public class Fenetre extends JFrame {
 
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
 
-public class Fenetre extends JFrame { 
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
-	public ImageSplit imgsplit = null;
+    public ImageSplit imgsplit = null;
     private ArrayList<moveThread> moveThreads;
 
-	Fenetre(){
-		final JPanel contentPane;
-		final JPanel affichage;
+    Fenetre() {
+        final JPanel contentPane;
+        final JPanel affichage;
         moveThreads = new ArrayList<moveThread>();
-		setTitle("Image"); 
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-		setBounds(0, 0, 1200, 750); 
-		setLocationRelativeTo(null);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		affichage = new JPanel();
-		//affichage.setBorder(new EmptyBorder(5, 5, 5, 5));
-		affichage.setBounds(100, 10, 1000 , 650);
-		
-		//affichage.setBorder(BorderFactory.createLineBorder(Color.black));
-		affichage.setLayout(null);
-		contentPane.add(affichage);
-		JButton btn_upload = new JButton("Upload");
+        setTitle("Image");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(0, 0, 1200, 750);
+        setLocationRelativeTo(null);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+        affichage = new JPanel();
+        //affichage.setBorder(new EmptyBorder(5, 5, 5, 5));
+        affichage.setBounds(100, 10, 1000, 650);
+
+        //affichage.setBorder(BorderFactory.createLineBorder(Color.black));
+        affichage.setLayout(null);
+        contentPane.add(affichage);
+        JButton btn_upload = new JButton("Upload");
         JButton btn_launch = new JButton("Lancer");
         btn_launch.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-            	if(imgsplit!=null){
-            	if(moveThreads.size()==0){
-            		Animation_launched(evt);
-            	}
-            	else if(moveThreads.get(0).isInterrupted()==false){
-            		for(int i=0;i<moveThreads.size();i++){
-            			moveThreads.get(i).reprise();
-            		}
-            	}
-            }
+                if (imgsplit != null) {
+                    if (moveThreads.size() == 0) {
+                        Animation_launched(evt);
+                    } else if (moveThreads.get(0).getInterrupted() == true) {
+                        for (int i = 0; i < moveThreads.size(); i++) {
+                            synchronized (moveThreads.get(i)){
+                                moveThreads.get(i).setInterrupted(false);
+                                moveThreads.get(i).notify();
+                            }
+                        }
+                    }
+                }
             }
         });
 
@@ -71,38 +71,37 @@ public class Fenetre extends JFrame {
             }
         });
 
-		btn_upload.addActionListener(new ActionListener()
-			{
-				public void actionPerformed(ActionEvent arg0)
-				{	
-					
-					String load=upload();
-					if(load!="aucun fichier selectionné"){
-						if(imgsplit!=null){
-							for(int i=0;i<imgsplit.images.length;i++){
-								contentPane.remove(imgsplit.images[i]);
-							}
-						}
-						try {
-							imgsplit = new ImageSplit(load,affichage);
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-                     
-						repaint();
-				}
-				}
-			});	
+        btn_upload.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
 
-		btn_upload.setBounds(50,670, 125, 30);
-        btn_launch.setBounds(400,670, 125, 30);
-        btn_pause.setBounds(700,670, 125, 30);
-		contentPane.add(btn_upload,0);
-        contentPane.add(btn_launch,0);
-        contentPane.add(btn_pause,0);
-	}
-	private String upload() {
+                String load = upload();
+                if (load != "aucun fichier selectionnï¿½") {
+                    if (imgsplit != null) {
+                        for (int i = 0; i < imgsplit.images.length; i++) {
+                            contentPane.remove(imgsplit.images[i]);
+                        }
+                    }
+                    try {
+                        imgsplit = new ImageSplit(load, affichage);
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+
+                    repaint();
+                }
+            }
+        });
+
+        btn_upload.setBounds(50, 670, 125, 30);
+        btn_launch.setBounds(400, 670, 125, 30);
+        btn_pause.setBounds(700, 670, 125, 30);
+        contentPane.add(btn_upload, 0);
+        contentPane.add(btn_launch, 0);
+        contentPane.add(btn_pause, 0);
+    }
+
+    private String upload() {
         JFileChooser chooser = new JFileChooser();
         chooser.setDialogType(JFileChooser.OPEN_DIALOG);
         int returnVal = chooser.showOpenDialog(getContentPane());
@@ -111,42 +110,40 @@ public class Fenetre extends JFrame {
             File ficChoisi = chooser.getSelectedFile();
             chemin = ficChoisi.getAbsolutePath();
             System.out.println("Chargement du fichier : " + ficChoisi.getAbsolutePath());
-   
-            }
+
+        }
         if (chemin == null) {
             return "aucun fichier selectionn?";
-        }
-        else {
+        } else {
             return chemin;
         }
-	}
-
-    private void Animation_launched (java.awt.event.MouseEvent evt){
-    	boolean direction=true;
-    	for(int i=0;i<3;i++){
-    		moveThread move;
-    		
-    		if((direction==true)){
-    			move = new moveThread((JPanel)imgsplit.images[i],(JPanel)imgsplit.images[i+3],true);
-    			direction=false;
-    		}
-    		else{
-    			move = new moveThread((JPanel)imgsplit.images[i],(JPanel)imgsplit.images[i+3],false);
-    			direction=true;
-    		}
-    		if(i==2){
-    			direction=true;
-    		}
-        
-		moveThreads.add(move);
-        move.start();
-    	}
-    	System.out.println();
     }
 
-    private void Animation_paused (java.awt.event.MouseEvent evt){
-        for( moveThread thread : moveThreads){
-            thread.interrupt();
+    private void Animation_launched(java.awt.event.MouseEvent evt) {
+        boolean direction = true;
+        for (int i = 0; i < 3; i++) {
+            moveThread move;
+
+            if ((direction == true)) {
+                move = new moveThread((JPanel) imgsplit.images[i], (JPanel) imgsplit.images[i + 3], true);
+                direction = false;
+            } else {
+                move = new moveThread((JPanel) imgsplit.images[i], (JPanel) imgsplit.images[i + 3], false);
+                direction = true;
+            }
+            if (i == 2) {
+                direction = true;
+            }
+
+            moveThreads.add(move);
+            move.start();
+        }
+        System.out.println();
+    }
+
+    private synchronized void Animation_paused(java.awt.event.MouseEvent evt){
+        for (moveThread thread : moveThreads) {
+            thread.setInterrupted(true);
         }
 
     }
--- trunk/ImageSplitTest/src/moveThread.java
+++ trunk/ImageSplitTest/src/moveThread.java
@@ -5,55 +5,60 @@ import java.awt.*;
 /**
  * Created by Gautier on 14/01/2015.
  */
-public class moveThread extends Thread{
+public class moveThread extends Thread {
 
     private JPanel target;
     private JPanel target2;
     private boolean interrupted;
     private boolean up;
 
-    public moveThread(JPanel _target,JPanel _target2,boolean up) {
+    public moveThread(JPanel _target, JPanel _target2, boolean up) {
         this.target = _target;
         this.target2 = _target2;
         this.interrupted = false;
-        this.up=up;
+        this.up = up;
     }
 
     @Override
     public synchronized void run() {
-        while (!this.interrupted) {
-            try {
-            	if(up==true){
-            		this.target.setLocation(this.target.getX(), (this.target.getY()+1));
-            		this.target2.setLocation(this.target2.getX(), (this.target2.getY()+1));
-            		if(this.target.getY()>this.target.getHeight()){
-            			this.target.setLocation(this.target.getX(),-this.target.getHeight());
-            		}
-            		if(this.target2.getY()>this.target2.getHeight()){
-            			this.target2.setLocation(this.target2.getX(),-this.target2.getHeight());
-            		}
-            	}
-            	else{
-            		this.target.setLocation(this.target.getX(), (this.target.getY()-1));
-            		this.target2.setLocation(this.target2.getX(), (this.target2.getY()-1));
-            		if(this.target.getY()<-this.target.getHeight()){
-            			this.target.setLocation(this.target.getX(),this.target.getHeight());
-            		}
-            		if(this.target2.getY()<-this.target2.getHeight()){
-            			this.target2.setLocation(this.target2.getX(),this.target2.getHeight());
-            		}
-            	}
+            while (true) {
+                try {
+                    if (up == true) {
+                        this.target.setLocation(this.target.getX(), (this.target.getY() + 1));
+                        this.target2.setLocation(this.target2.getX(), (this.target2.getY() + 1));
+                        if (this.target.getY() > this.target.getHeight()) {
+                            this.target.setLocation(this.target.getX(), -this.target.getHeight());
+                        }
+                        if (this.target2.getY() > this.target2.getHeight()) {
+                            this.target2.setLocation(this.target2.getX(), -this.target2.getHeight());
+                        }
+                    } else {
+                        this.target.setLocation(this.target.getX(), (this.target.getY() - 1));
+                        this.target2.setLocation(this.target2.getX(), (this.target2.getY() - 1));
+                        if (this.target.getY() < -this.target.getHeight()) {
+                            this.target.setLocation(this.target.getX(), this.target.getHeight());
+                        }
+                        if (this.target2.getY() < -this.target2.getHeight()) {
+                            this.target2.setLocation(this.target2.getX(), this.target2.getHeight());
+                        }
+                    }
+                    this.sleep(10);
+                    synchronized (this) {
+                        while (this.interrupted)
+                            wait();
+                    }
+                }
+                 catch (InterruptedException e) {
 
-                this.sleep(10);
-            } catch (Exception e) {
-               
-            }
+                 }
         }
+
     }
 
-    public void pause(){
-       }
-    public void reprise(){
-        this.interrupted = false;
+    public void setInterrupted(boolean value) {
+        this.interrupted = value;
+    }
+    public boolean getInterrupted() {
+        return this.interrupted;
     }
 }
